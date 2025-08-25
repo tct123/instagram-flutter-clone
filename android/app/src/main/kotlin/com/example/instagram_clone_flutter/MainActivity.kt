83b377f2af9e15421f72ef5d08d8f93ba3db954a
tct123 <42028373+tct123@users.noreply.github.com>
2025-08-25 06:10:18 +0200

    .

--- android/app/src/main/kotlin/com/example/instagram_clone_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/instagram_clone_flutter/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.instagram_clone_flutter
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
